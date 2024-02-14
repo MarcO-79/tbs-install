025e5620e8f72e09df6084611c63d62d57af79df
Marcin Bielicki <91062129+MarcO-79@users.noreply.github.com>
2024-02-14 12:16:50 +0100

    Update drv-tbs.sh

--- drv-tbs.sh
+++ drv-tbs.sh
@@ -24,9 +24,9 @@ fi
 
 systemctl disable apt-daily.service
 systemctl disable apt-daily.timer
-rm -rf /usr/src/media_build /usr/src/dvb-firmwares.tar.bz2
-cd /usr/src
-git clone https://github.com/tbsdtv/media_build.git
+#rm -rf /usr/src/media_build /usr/src/dvb-firmwares.tar.bz2
+#cd /usr/src
+#git clone https://github.com/tbsdtv/media_build.git
 
 elif [ -n "`which yum`" ]; then
         yum -y group install "Development Tools"
@@ -45,11 +45,10 @@ git clone https://github.com/tbsdtv/media_build.git
 sed -i '/vm_fault_t;/d' /usr/src/media_build/v4l/compat.h
 sed -i '/add v4.20_access_ok.patch/d' /usr/src/media_build/backports/backports.txt
 fi
-###### install fimware
 
+###### install fimware
 curl -L https://github.com/tbsdtv/media_build/releases/download/latest/dvb-firmwares.tar.bz2 | \
     tar -jxf - -C /lib/firmware/
-
 
 cd /usr/src/media_build
 ./build
